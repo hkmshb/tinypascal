9302f1b6f63d2c566a0cd7ed13521dda11fa63a8
Abdul-Hakeem Shaibu <hkmshb@gmail.com>
2018-08-26 11:17:17 +0100

    rs: Read and print user input to standard output

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,35 +1,14 @@
-extern crate rand;
-
-use std::io;
-use std::cmp::Ordering;
-use rand::Rng;
+use std::io::{self, Write};
 
 
 fn main() {
-    println!("Guess the number!");
+    let mut text: String = String::new();
 
-    let secret_number = rand::thread_rng().gen_range(1, 101);
-    println!("The secret number is: {}", secret_number);
+    print!("calc> ");
+    io::stdout().flush();
 
-    loop {
-        println!("\nPlease input your guess.");
-        let mut guess = String::new();
-        io::stdin().read_line(&mut guess)
-            .expect("Failed to read line");
+    io::stdin().read_line(&mut text)
+        .expect("Failed to read line");
 
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
-
-        println!("You guessed: {}", guess);
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => {
-                println!("You win");
-                break;
-            },
-        }
-    }
+    println!("Entered expression: {}", text);
 }
