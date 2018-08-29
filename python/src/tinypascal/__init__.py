062e93bee567ff6fd9bbe16f09a8f8d8c7446d22
Abdul-Hakeem Shaibu <hkmshb@gmail.com>
2018-08-29 22:39:20 +0100

    Walk and print AST nodes returned by Parser class

--- python/src/tinypascal/__init__.py
+++ python/src/tinypascal/__init__.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 import sys
-import enum
 import pkg_resources
+from .lexer import Lexer
+from .parser import Parser
+
 
 
 def get_version():
@@ -11,171 +13,13 @@ def get_version():
     return packages[0].version
 
 
-class TokenType(enum.Enum):
-    EOF = "EOF"
-    LPAREN = "("
-    RPAREN = ")"
-    INTEGER = "INTEGER"
-    MINUS = "MINUS"
-    PLUS = "PLUS"
-    DIV = "DIV"
-    MUL = "MUL"
-
-
-class Token:
-
-    def __init__(self, type, value):
-        self.type = type
-        self.value = value
-
-    def __str__(self):
-        '''String representation of class instance.
-
-        Examples:
-            Token(TokenType.INTEGER, 3)
-            Token(TokenType.PLUS, '+')
-            Token(TokenType.DIV, '/')
-        '''
-        return "Token ({type}, {value})".format(
-            type=self.type, value=self.value
-        )
-
-    def __repr__(self):
-        return self.__str__()
-
-
-class Lexer:
-
-    def __init__(self, text):
-        # client string input, e.g: "3 * 5", "9 + 2 - 3 * 5"
-        self.text = text
-        # self.pos is index into client string input, self.text
-        self.pos = 0
-        self.current_char = text[self.pos]
-
-    def error(self):
-        raise Exception("Invalid character")
-
-    def advance(self):
-        '''Advance the `pos` pointer and set the `current_char` variable.
-        '''
-        self.pos += 1
-        if self.pos > len(self.text) - 1:
-            self.current_char = None
-        else:
-            self.current_char = self.text[self.pos]
-
-    def skip_whitespace(self):
-        while self.current_char is not None and self.current_char.isspace():
-            self.advance()
-
-    def get_integer(self):
-        '''Return a multidigit integer consumed from the input.
-        '''
-        result = ''
-        while self.current_char is not None and self.current_char.isdigit():
-            result += self.current_char
-            self.advance()
-        return int(result)
-
-    def get_next_token(self):
-        '''Lexical analyser (aka scanner or tokenizer)
-
-        This method is repsonsible for breaking an expression apart into
-        tokens. One token at a time.
-        '''
-        while self.current_char is not None:
-            if self.current_char.isspace():
-                self.skip_whitespace()
-                continue
-
-            if self.current_char.isdigit():
-                return Token(TokenType.INTEGER, self.get_integer())
-
-            if self.current_char in '+-*/()':
-                token_type = (
-                    TokenType.PLUS if self.current_char == '+' else
-                    TokenType.MINUS if self.current_char == '-' else
-                    TokenType.MUL if self.current_char == '*' else
-                    TokenType.DIV if self.current_char == '/' else
-                    TokenType.LPAREN if self.current_char == '(' else
-                    TokenType.RPAREN
-                )
-                token = Token(token_type, self.current_char)
-                self.advance()
-                return token
-
-            self.error()
-        return Token(TokenType.EOF, None)
-
-
 class Interpreter:
 
-    def __init__(self, lexer):
-        self.lexer = lexer
-        self.current_token = lexer.get_next_token()
+    def __init__(self, parser):
+        self.parser = parser
 
-    def error(self, error_code=0):
-        raise Exception('Error parsing input')
-
-    def eat(self, token_type):
-        # compares the current token with the passed in token type and if
-        # they match then `eat` the current token and assign the ntext
-        # token ...
-        if self.current_token.type == token_type:
-            self.current_token = self.lexer.get_next_token()
-        else:
-            self.error()
-
-    def factor(self):
-        '''factor : INTEGER | LPAREN expr RPAREN
-        '''
-        token = self.current_token
-        if token.type == TokenType.INTEGER:
-            self.eat(TokenType.INTEGER)
-            return token.value
-        elif token.type == TokenType.LPAREN:
-            self.eat(TokenType.LPAREN)
-            value = self.expr()
-            self.eat(TokenType.RPAREN)
-            return value
-
-    def term(self):
-        '''term: factor ((MUL | DIV) factor)*
-        '''
-        result = self.factor()
-        while self.current_token.type in (TokenType.MUL, TokenType.DIV):
-            token = self.current_token
-            if token.type == TokenType.MUL:
-                self.eat(TokenType.MUL)
-                result *= self.factor()
-            elif token.type == TokenType.DIV:
-                self.eat(TokenType.DIV)
-                result /= self.factor()
-        return result
-
-
-    def expr(self):
-        '''Arithmetic expression parser / interpreter.
-
-        calc> 9 + 8 - 3 * 5
-        2
-
-        expr:   term ((PLUS | MINUS) term)*
-        term:   factor ((MUL | DIV) factor)*
-        factor: INTEGER
-        '''
-        result = self.term()
-
-        while self.current_token.type in (TokenType.PLUS, TokenType.MINUS):
-            token = self.current_token
-            if token.type == TokenType.PLUS:
-                self.eat(TokenType.PLUS)
-                result += self.term()
-            elif token.type == TokenType.MINUS:
-                self.eat(TokenType.MINUS)
-                result -= self.term()
-        return result
+    def run(self):
+        return self.parser.parse()
 
 
 def main():
@@ -188,9 +32,26 @@ def main():
             continue
 
         try:
-            interpreter = Interpreter(Lexer(text))
-            result = interpreter.expr()
-            print(result)
+            parser = Parser(Lexer(text))
+            interpreter = Interpreter(parser)
+            tree = interpreter.run()
+            print_tree(tree)
         except Exception as ex:
             print('error: {}'.format(ex))
+            raise ex
             sys.exit()
+
+
+def print_tree(root):
+    from .parser import BinOp, Num
+
+    def walk(node):
+        if isinstance(node, BinOp):
+            print(node.op.value)
+            walk(node.left)
+            walk(node.right)
+            print()
+        else:
+            print("%s " % node.value, end='')
+
+    walk(root)
