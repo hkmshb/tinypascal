0c9dc6794d076c01572af19d47c631f0259e5201
Abdul-Hakeem Shaibu <hkmshb@gmail.com>
2018-08-30 03:30:35 +0100

    Update Parser to support new AST nodes to match pascal lang tokens

--- python/src/tinypascal/parser.py
+++ python/src/tinypascal/parser.py
@@ -1,7 +1,6 @@
 from .lexer import TokenType
 
 
-
 class AST:
     '''Base class for all nodes within an Abstract-Syntax Tree.
     '''
@@ -27,6 +26,30 @@ class Num(AST):
         self.value = token.value
 
 
+class Compound(AST):
+    '''Represents a 'BEGIN ... END' block.
+    '''
+    def __init__(self):
+        self.children = []
+
+
+class Assign(AST):
+    def __init__(self, left, op, right):
+        self.left = left
+        self.token = self.op = op
+        self.right = right
+
+
+class Var(AST):
+    '''The Var node is constructed out of ID token.
+    '''
+    def __init__(self, token):
+        self.token = token
+        self.value = token.value
+
+
+class NoOp(AST):
+    pass
 
 
 class Parser:
@@ -48,7 +71,11 @@ class Parser:
             self.error()
 
     def factor(self):
-        '''factor : (PLUS | MINUS) factor | INTEGER | LPAREN expr RPAREN
+        '''factor : PLUS factor
+                  | MINUS factor
+                  | INTEGER
+                  | LPAREN expr RPAREN
+                  | variable
         '''
         token = self.current_token
         if token.type == TokenType.PLUS:
@@ -60,11 +87,13 @@ class Parser:
         if token.type == TokenType.INTEGER:
             self.eat(TokenType.INTEGER)
             return Num(token)
-        elif token.type == TokenType.LPAREN:
+        if token.type == TokenType.LPAREN:
             self.eat(TokenType.LPAREN)
             node = self.expr()
             self.eat(TokenType.RPAREN)
             return node
+        else:
+            return self.variable()
 
     def term(self):
         '''term: factor ((MUL | DIV) factor)*
@@ -99,5 +128,75 @@ class Parser:
             node = BinOp(node, token, self.term())
         return node
 
+    def empty(self):
+        '''An empty production.
+        '''
+        return NoOp()
+
+    def variable(self):
+        '''variable : ID
+        '''
+        token = self.current_token
+        self.eat(TokenType.ID)
+        return Var(token)
+
+    def assignment_statement(self):
+        '''assignment_statement : variable ASSIGN expr
+        '''
+        left = self.variable()
+        token = self.current_token
+        self.eat(TokenType.ASSIGN)
+        right = self.expr()
+        return Assign(left, token, right)
+
+    def statement(self):
+        '''statement : compound_statement
+                     | assignment_statement
+                     | empty
+        '''
+        if self.current_token.type == TokenType.BEGIN:
+            node = self.compound_statement()
+        elif self.current_token.type == TokenType.ID:
+            node = self.assignment_statement()
+        else:
+            node = self.empty()
+        return node
+
+    def statement_list(self):
+        '''statement_list : statement
+                          | statement SEMI statement_list
+        '''
+        results = [self.statement()]
+        while self.current_token.type == TokenType.SEMI:
+            self.eat(TokenType.SEMI)
+            results.append(self.statement())
+
+        if self.current_token.type == TokenType.ID:
+            self.error()
+
+        return results
+
+    def compound_statement(self):
+        '''compound_statement: BEGIN statement_list END
+        '''
+        self.eat(TokenType.BEGIN)
+        nodes = self.statement_list()
+        self.eat(TokenType.END)
+
+        root = Compound()
+        for node in nodes:
+            root.children.append(node)
+        return root
+
+    def program(self):
+        '''program: compound_statement DOT
+        '''
+        node = self.compound_statement()
+        self.eat(TokenType.DOT)
+        return node
+
     def parse(self):
-        return self.expr()
+        node = self.program()
+        if self.current_token.type != TokenType.EOF:
+            self.error()
+        return node
